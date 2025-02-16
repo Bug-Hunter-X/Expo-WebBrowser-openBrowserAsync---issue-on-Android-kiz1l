8ec541fe6e1a1c2f5dc08959c501f79eb68d6f1c
BugHunter-X <askzenai@gmail.com>
2025-02-16 13:48:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+import * as WebBrowser from 'expo-web-browser';
+
+// ... other code ...
+
+const handlePress = async () => {
+  let result = await WebBrowser.openBrowserAsync('https://example.com');
+  console.log('Result: ', result);
+};
+
+// ... rest of the component ...
